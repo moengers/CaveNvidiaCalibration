71f2f81a86e1e484e12c0ec5eef3f860ab67f91b
MoEngers <moritz.engers@student.htw-berlin.de>
2019-06-23 22:35:56 +0100

    Update CalibrationMenu.cs

--- CaveNvidiaCalibration/Assets/UI/CalibrationMenu/CalibrationMenu.cs
+++ CaveNvidiaCalibration/Assets/UI/CalibrationMenu/CalibrationMenu.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class CalibrationMenu : MonoBehaviour
 {
-    public GameObject CanvasPrefab;
-    private Canvas MenuCanvas;
-
+  
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(CanvasPrefab, FindObjectOfType<Canvas>().transform);
     }
 
     // Update is called once per frame
